b92dc8b01ff1cc5175b54f5d2a03b17fdeb5e648
Michael Starovoitov <mihatestform@gmail.com>
2024-07-12 16:10:10 +0300

    code refactoring

--- data/config.py
+++ data/config.py
@@ -1,4 +1,4 @@
-OPENAI_API_KEY = "sk-proj-4hJqn21HaLndDeIJqWSFT3BlbkFJZO983Nmmi23EoBrAkhuX"
+OPENAI_API_KEY = ""
 AssistantID = "asst_COt7IUWH96AQMZVBmw6eykuu"
 MODEL = "gpt-4-turbo-preview"
 TOKEN = '6888113788:AAH1p-QFWpT-r077I_Wv3eIwXzzD658rCXY'
